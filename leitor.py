0149815fd1e3ebc57d483be217071a0673d3050a
niceias.filho <niceias.rn@hotmail.com>
2022-10-05 08:28:10 -0300

    Limpeza de Tela, ativação do modelo de gabarito adaptado

--- leitor.py
+++ leitor.py
@@ -1,79 +1,100 @@
 import os
+import sys
 
-print("Escolha o Modelo de Gabarito")
+print("Escolha o Modelo de Gabarito: ")
 print("Regular (1)" "\n" 'Adaptada (2)' '\n'"Olimpiada (3)" '\n' "Olimpiada Adapatada (4)")
-gab = input('Digite seu modelo de Gabarito: ')
-caminho = input("digite o caminho do arquivo: ")
 
-while True:
-    doc = input("Nome do txt: ")
-    arquivo = open(caminho + doc + ".txt")
-    leitura = arquivo.readlines()
+gab = input('Informe o modelo de Gabarito: ')
+gab = int(gab)
+
+if(gab > 4):
+   sys.exit("Programa encerrado por digitar um valor invalido!!")
+else:
+    caminho = input("digite o caminho do arquivo: ")
+    destino = input("Informe o destino do arquivo ")
+
+    while True:
+        print("################################################")
+        print(f'Origem: {caminho}')
+        print(f'Destino: {destino}')
+        print("################################################")
+        doc = input("Nome do txt: ")
+        arquivo = open(caminho + "\\" + doc + ".txt")
+        leitura = arquivo.readlines()
 
 
-    lista = []
-    nlista = []
-    nova = []
-    proc = []
+        lista = []
+        nlista = []
+        nova = []
+        proc = []
 
-    for valor in leitura:
-        lista.append(valor)
+        for valor in leitura:
+            lista.append(valor)
 
 
-    if (gab == '1'):
-        for item in lista:
-            a = item[0:19]
-            b = item[19:29]
-            c = item[29:]
-            car = ';A;'
-            ponto = ';'
-            nova = (a + ponto + b + car + c)
-            proc.append(nova)
-
-        with open ("C:\\Users\\Niceias\\Desktop\\limpos\\" + doc + '.txt', 'w') as arquivo:
-            for valor in proc:
-                arquivo.write(valor.strip() + '\n')
-
-    elif(gab == '2'):
-        for item in lista:
-            a = item[0:19]
-            b = item[19:29]
-            c = item[29:]
-            car = ';A;'
-            caradp = 'A;'
-            ponto = ';'
-            nova = (a + caradp + b + car + c)
-            proc.append(nova)
-
-        with open("C:\\Users\\Niceias\\Desktop\\limpos\\" + doc + '.txt', 'w') as arquivo:
-            for valor in proc:
-                arquivo.write(valor.strip() + '\n')
-
-    elif (gab == '3'):
+        if (gab == 1):
             for item in lista:
-                a = item[0:20]
-                b = item[20:30]
-                c = item[30:]
+                a = item[0:19]
+                b = item[19:29]
+                c = item[29:]
                 car = ';A;'
                 ponto = ';'
                 nova = (a + ponto + b + car + c)
                 proc.append(nova)
 
-            with open("C:\\Users\\Niceias\\Desktop\\limpos\\" + doc + '.txt', 'w') as arquivo:
+
+            with open(destino + "\\" + doc + '.txt', 'w') as arquivo:
                 for valor in proc:
-                   arquivo.write(valor.strip() + '\n')
+                    arquivo.write(valor.strip() + '\n')
+            os.system('cls')
 
-    elif (gab == '4'):
-        for item in lista:
-            a = item[0:20]
-            b = item[20:30]
-            c = item[30:]
-            car = ';A;'
-            caradp = 'A;'
-            ponto = ';'
-            nova = (a + caradp + b + car + c)
-            proc.append(nova)
+        elif(gab == 2):
+            for item in lista:
+                a = item[0:19]
+                b = item[19:29]
+                c = item[29:]
+                car = ';A;'
+                caradp = 'A;'
+                ponto = ';'
+                nova = (a + caradp + b + car + c)
+                proc.append(nova)
 
-        with open("C:\\Users\\Niceias\\Desktop\\limpos\\" + doc + '.txt', 'w') as arquivo:
-            for valor in proc:
-                arquivo.write(valor.strip() + '\n')
+
+            with open(destino + "\\" + doc + '.txt', 'w') as arquivo:
+                for valor in proc:
+                    arquivo.write(valor.strip() + '\n')
+            os.system('cls')
+
+        elif (gab == 3):
+                for item in lista:
+                    a = item[0:20]
+                    b = item[20:30]
+                    c = item[30:]
+                    car = ';A;'
+                    ponto = ';'
+                    nova = (a + ponto + b + car + c)
+                    proc.append(nova)
+
+
+                with open(destino + "\\" + doc + '.txt', 'w') as arquivo:
+                    for valor in proc:
+                       arquivo.write(valor.strip() + '\n')
+                os.system('cls')
+
+        elif (gab == 4):
+            for item in lista:
+                a = item[0:20]
+                b = item[20:30]
+                c = item[30:]
+                car = ';A;'
+                caradp = 'A;'
+                ponto = ';'
+                nova = (a + caradp + b + car + c)
+                proc.append(nova)
+
+
+            with open(destino + "\\" + doc + '.txt', 'w') as arquivo:
+                for valor in proc:
+                    arquivo.write(valor.strip() + '\n')
+            os.system('cls')
+
