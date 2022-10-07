b609b51d7d3721d54e99e5da5ebf3a526fb5da6c
niceias.filho <niceias.rn@hotmail.com>
2022-10-07 11:25:27 -0300

    Melhorias no código

--- leitor.py
+++ leitor.py
@@ -7,17 +7,40 @@ print("Regular (1)" "\n" 'Adaptada (2)' '\n'"Olimpiada (3)" '\n' "Olimpiada Adap
 gab = input('Informe o modelo de Gabarito: ')
 gab = int(gab)
 
+gabarito_regular = "Modelo de Gabarito: REGULAR"
+gabarito_regular_adaptado = "Modelo de Gabarito: REGULAR ADAPTADO"
+gabarito_olimpiada = "Modelo de Gabarito: OLIMPIADA"
+gabarito_olimpiada_adaptado = "Modelo de Gabarito: OLIMPIADA ADAPTADO"
+gabarito_linguagens = "Modelo de Gabarito: LÍNGUA ESTRANGEIRA"
+
+if (gab == 1):
+    gabarito_regular
+
 if(gab > 5):
    sys.exit("Programa encerrado por digitar um valor invalido!!")
 else:
-    caminho = input("digite o caminho do arquivo: ")
-    destino = input("Informe o destino do arquivo ")
+    caminho = input("Origem do arquivo: ")
+    destino = input("Destino do arquivo ")
 
     while True:
         print("################################################")
+        print(f'Editor de TXT de Gabaritos')
+        print(f'  *** COLÉGIO MASTER ***')
         print(f'Origem: {caminho}')
         print(f'Destino: {destino}')
-        print("################################################")
+
+        if (gab == 1):
+            print(gabarito_regular)
+        elif (gab == 2):
+            print(gabarito_regular_adaptado)
+        elif (gab == 3):
+            print(gabarito_olimpiada)
+        elif (gab == 4):
+            print(gabarito_olimpiada_adaptado)
+        else:
+            print(gabarito_linguagens)
+        print("#################################################")
+
         doc = input("Nome do txt: ")
         arquivo = open(caminho + "\\" + doc + ".txt")
         leitura = arquivo.readlines()
@@ -31,118 +54,71 @@ else:
         for valor in leitura:
             lista.append(valor)
 
-
+        #TODO: GABARITO REGULAR
         if (gab == 1):
-            for item in lista:
-                a = item[0:19]
-                b = item[19:29]
-                c = item[29:]
-                car = ';A;'
-                ponto = ';'
-                nova = (a + ponto + b + car + c)
-                proc.append(nova)
 
+            for item in lista:
+                nova = item[0:19] + ";" + item[19:29] + ";A;" + item[29:]
+                proc.append(nova)
 
             with open(destino + "\\" + doc + '.txt', 'w') as arquivo:
                 for valor in proc:
                     arquivo.write(valor.strip() + '\n')
             os.system('cls')
 
+        #TODO: GABARITO REGULAR ADAPTADO
         elif(gab == 2):
+
             for item in lista:
-                a = item[0:19]
-                b = item[19:29]
-                c = item[29:]
-                car = ';A;'
-                caradp = 'A;'
-                ponto = ';'
-                nova = (a + caradp + b + car + c)
+                nova = item[0:19] + "A;" + item[19:29] + ";A;" + item[29:]
                 proc.append(nova)
 
+            with open(destino + "\\" + doc + '.txt', 'w') as arquivo:
+                 for valor in proc:
+                    arquivo.write(valor.strip() + '\n')
+            os.system('cls')
+
+        #TODO: GABARITO DE OLIMPIADA
+        elif (gab == 3):
+
+            for item in lista:
+                nova = item[0:20] + ";" + item[20:30] + ";A;" + item[30:]
+                proc.append(nova)
 
             with open(destino + "\\" + doc + '.txt', 'w') as arquivo:
                 for valor in proc:
                     arquivo.write(valor.strip() + '\n')
             os.system('cls')
 
-        elif (gab == 3):
-                for item in lista:
-                    a = item[0:20]
-                    b = item[20:30]
-                    c = item[30:]
-                    car = ';A;'
-                    ponto = ';'
-                    nova = (a + ponto + b + car + c)
-                    proc.append(nova)
+        #TODO: GABARITO DE OLIMPIADA ADAPTADA
+        elif (gab == 4):
+           
+            for item in lista:
+                nova = item[0:20] + "A;" + item[20:30] + ";A;" + item[30:]
+                proc.append(nova)
 
+            with open(destino + "\\" + doc + '.txt', 'w') as arquivo:
+                for valor in proc:
+                    arquivo.write(valor.strip() + '\n')
+            os.system('cls')
+
+        #TODO: GABARITO DE LINGUAS ESTRANGEIRAS
+        elif (gab == 5):
+            for item in leitura:
+
+                asterisco = item[0:30]
+
+                if (asterisco[29] == "*"):
+                    asterisco = asterisco.replace("*", ";")
+                    asterisco = asterisco[0:19] + ";" + asterisco[19:29] + ";" + item[30:33] + ";" + "A;" + item[33:]
+                    proc.append(asterisco)
+                else:
+                    asterisco = item[0:19] + ";" + item[19:29] + ";" + item[29:32] + ";" + "A;" + item[32:]
+                    proc.append(asterisco)
 
                 with open(destino + "\\" + doc + '.txt', 'w') as arquivo:
                     for valor in proc:
-                       arquivo.write(valor.strip() + '\n')
+                        arquivo.write(valor.strip() + '\n')
                 os.system('cls')
 
-        elif (gab == 4):
-            for item in lista:
-                a = item[0:20]
-                b = item[20:30]
-                c = item[30:]
-                car = ';A;'
-                caradp = 'A;'
-                ponto = ';'
-                nova = (a + caradp + b + car + c)
-                proc.append(nova)
 
-
-            with open(destino + "\\" + doc + '.txt', 'w') as arquivo:
-                for valor in proc:
-                    arquivo.write(valor.strip() + '\n')
-            os.system('cls')
-
-        elif (gab == 5):
-            for item in leitura:
-                asterisco = item[19:30]
-
-                if (asterisco[10] == "*"):
-
-                    esp = asterisco.replace("*", ";ESP;")
-
-                    a = item[0:20]
-                    b = item[20:30]
-                    c = item[33:]
-                    car = ';A;'
-                    caradp = 'A;'
-                    ponto = ';'
-                    nova = (a + ponto + esp + caradp + c)
-                    proc.append(nova)
-
-
-                    with open(destino + "\\" + doc + '.txt', 'w') as arquivo:
-                        for valor in proc:
-                            arquivo.write(valor.strip() + '\n')
-                    os.system('cls')
-
-                else:
-
-
-
-                    a = item[0:20]
-                    b = item[20:30]
-                    c = item[30:33]
-                    d = item[34:]
-                    car = ';A;'
-                    caradp = 'A;'
-                    ponto = ';'
-                    #ing = ';ING'
-                    nova = (a + ponto + b + ponto + c + car + d)
-                    proc.append(nova)
-
-                    with open(destino + "\\" + doc + '.txt', 'w') as arquivo:
-                        for valor in proc:
-                            arquivo.write(valor.strip() + '\n')
-                    os.system('cls')
-
-
-                    with open(destino + "\\" + doc + '.txt', 'w') as arquivo:
-                        for valor in proc:
-                            arquivo.write(valor.strip() + '\n')
-                    os.system('cls')
